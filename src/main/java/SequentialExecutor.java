20a3c256f823495cfa9eda2e6bd2930a2db0866d
[luismpfranco] <[lmiguelpf02@hotmail.com]>
2024-03-17 14:58:47 +0000

    feat(SequentialExecutor): implement sequential executor

---
+++ src/main/java/SequentialExecutor.java
@@ -0,0 +1,42 @@
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.Map;
+
+public class SequentialExecutor {
+    private final BufferedImage image;
+    private final int nCols;
+    private final int nRows;
+    private final int width;
+    private final int height;
+    private final int type;
+    private BufferedImage[][] splitImages;
+
+    public SequentialExecutor(BufferedImage image, int nCols, int nRows, int width, int height, int type) {
+        this.image = image;
+        this.nCols = nCols;
+        this.nRows = nRows;
+        this.width = width;
+        this.height = height;
+        this.type = type;
+        splitImages = new BufferedImage[nRows][nCols];
+    }
+
+    public void execute(Server server, ImagePart imagePart){
+        Client client = new Client("Client A", new LoadInfo("load.info"), nRows, nCols);
+        Request request = new Request("greeting", "Hello, Server!", imagePart.getImage());
+        Response response = client.sendRequestAndReceiveResponse("localhost", server.getPort(), request);
+        BufferedImage imageWithoutRed = removeRed(ImageTransformer.createImageFromBytes(response.getImageSection()));
+        imagePart.setImage(imageWithoutRed);
+    }
+
+    private BufferedImage removeRed(BufferedImage image) {
+        for (int x = 0; x < image.getWidth(); x++) {
+            for (int y = 0; y < image.getHeight(); y++) {
+                Color color = new Color(image.getRGB(x, y));
+                Color newColor = new Color(0, color.getGreen(), color.getBlue());
+                image.setRGB(x, y, newColor.getRGB());
+            }
+        }
+        return image;
+    }
+}
