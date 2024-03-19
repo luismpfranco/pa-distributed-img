ba8bf4b9f95de3461b49124cbd61a3e97f073cfe
[luismpfranco] <[lmiguelpf02@hotmail.com]>
2024-03-19 20:28:59 +0000

    feat: create UserInterface class for single file selection

---
+++ src/main/java/UserInterface.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class UserInterface extends JPanel {
+    private JButton selectImageButton;
+    private JTextArea messageArea;
+    private JFileChooser fileChooser;
+
+    public UserInterface() {
+        this.setLayout(new FlowLayout());
+
+        // Create the select image button
+        selectImageButton = new JButton("Select Image");
+        selectImageButton.addActionListener(e -> {
+            fileChooser = new JFileChooser();
+            fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            fileChooser.setMultiSelectionEnabled(false);
+            int returnValue = fileChooser.showOpenDialog(null);
+            if (returnValue == JFileChooser.APPROVE_OPTION) {
+                Main.selectedFile = fileChooser.getSelectedFile();
+                messageArea.append("Image selected: " + Main.selectedFile.getName() + "\n");
+            }
+
+        });
+
+        messageArea = new JTextArea(5, 20);
+        messageArea.setEditable(false);
+
+        // Add components to the panel
+        this.add(selectImageButton);
+        this.add(new JScrollPane(messageArea));
+    }
+}
